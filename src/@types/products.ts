1f6e9c3b545c2ec212702b27f479c007a78976a3
Gustavo Uliano <gustavouliano11@gmail.com>
2024-09-28 14:08:14 -0300

    first commit

--- src/@types/products.ts
+++
@@ -1,9 +0,0 @@
-export type Product = {
-  id: number;
-  name: string;
-  description: string;
-  image: string;
-  price: number;
-  quantity: number;
-  productCategory: number;
-};
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,13 +1,6 @@
 import type { Metadata } from "next";
-import { AppRouterCacheProvider } from "@mui/material-nextjs/v14-appRouter";
 import "bootstrap/dist/css/bootstrap.min.css";
-import Sidebar, { SidebarItem } from "@/components/Layout/Sidebar";
 import "./globals.css";
-import { BarChart3, LayoutDashboard, Settings } from "lucide-react";
-import { getSession } from "@/lib/dal";
-import { useEffect } from "react";
-import { cookies } from "next/headers";
-import EmployeeSidebar from "@/components/Layout/Sidebar/employee-sidebar";
 export const metadata: Metadata = {
   title: {
     default: "Projeto Teste",
@@ -21,16 +14,12 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  const isLogged = getSession();
   return (
     <html lang="pt-BR">
       <body>
-        <AppRouterCacheProvider>
           <main className="App">
-            {isLogged && <EmployeeSidebar />}
             <div className="w-full">{children}</div>
           </main>
-        </AppRouterCacheProvider>
       </body>
     </html>
   );
